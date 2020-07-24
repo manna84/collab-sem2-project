2a2638e86efcb83dad9771e591969224e6a688a1
manna84 <60120742+manna84@users.noreply.github.com>
2020-07-24 10:30:01 -0400

    succefull post submisison

--- public/js/forum.js
+++ public/js/forum.js
@@ -3,4 +3,9 @@ const petData = document.getElementById('petForm');
 petData.addEventListener('submit', (event) => {
     event.preventDefault();
     console.log(petData.petDetail.value);
+    const petBox = document.getElementById('para');
+    petBox.innerHTML = petData.petDetail.value;
 });
+
+// const petBox = document.getElementsByClassName("para");
+// petBox.innerHTML = petData.petDetail.value;
