7036d51ca133bb265c19761bdaa61dc2ed2b0a26
manna84 <60120742+manna84@users.noreply.github.com>
2020-07-24 10:49:16 -0400

    succesfull apend li to UL to add multiple posts

--- public/js/forum.js
+++ public/js/forum.js
@@ -3,8 +3,15 @@ const petData = document.getElementById('petForm');
 petData.addEventListener('submit', (event) => {
     event.preventDefault();
     console.log(petData.petDetail.value);
-    const petBox = document.getElementById('para');
-    petBox.innerHTML = petData.petDetail.value;
+    // const petBox = document.getElementById('para');
+    // petBox.innerHTML = petData.petDetail.value;
+
+    const box = document.getElementById('para')
+    const textBox = document.createElement('li');
+
+    textBox.textContent = petData.petDetail.value;
+
+    box.append(textBox);
 });
 
 // const petBox = document.getElementsByClassName("para");
