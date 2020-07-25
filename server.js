e35ed927b8b18f6a1f30f061f8ea2fc3cef0748b
Nishant Malik <60120827+nishantmalik20@users.noreply.github.com>
2020-07-25 00:25:26 -0400

    server.js updated with chat feature

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const exphbs = require('express-handlebars');
-
+const PORT = process.env.PORT || 3000;
 const app = express();
+const io = require('socket.io').listen(server)
+const server = app.listen(port, () => {console.log(`Server running at http://localhost:${port}`)})
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -12,8 +14,39 @@ const generalController = require("./controllers/general");
 
 app.use("/", generalController);
 
-const PORT = process.env.PORT || 3000;
+
 
 app.listen(PORT, ()=>{
     console.log("Server is running");
 })
+
+////Chat Feature (Socket.io)
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + 'views/chat.handlebars')
+  })
+  
+  app.use(express.static('.'))
+  
+  
+  
+  
+  io.on('connection', function (socket) {
+      
+    // Listen for a "newuser" message
+    socket.on('newuser', (data) => {
+      // Transmit a message to everyone except the sender
+      //socket.broadcast.emit('newuser', data)
+  
+      // The same message, sent to all users - try it!
+      io.emit('newuser', data)
+      })
+       
+      
+      // Listen for "chatmsg"
+      //   io.emit to all user
+      socket.on('chatmsg', (data) => {
+      socket.broadcast.emit('chatmsg', data)
+      })
+  
+  
+  })
