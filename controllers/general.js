92dbdd622be9186f51894d48808afb8854a4ff07
Naveen Koul <koul1.naveen@gmail.com@gmail.com>
2020-07-24 22:05:04 -0400

    added validation in login page

--- controllers/general.js
+++ controllers/general.js
@@ -49,10 +49,43 @@ router.get("/login",(req, res)=>{
 
 router.post("/login",(req, res)=>{
 
-    res.render("login", {
-        title : "Login | BeMyPet"
-    })
+    const errors = [];
 
+    if (req.body.email == "") {
+        errors.push("Please enter an Email...!!!")
+    }
+
+    if (req.body.password == ""){
+        errors.push("Please enter a Password...!!!")
+    }
+
+    if (req.body.password.length > 0 && req.body.password.length < 6) {
+        errors.push("Password must contain atleast 6 characters")
+    }
+
+    const passValid = /^(?=.*\d.*)(?=.*[a-z].*)(?=.*[A-Z].*)(?=.*[@#%\*\-+=~\[\]{}<>\?].*)/
+
+    if (!req.body.password.match(passValid)) {
+        errors.push("Entered password must contain at least a special character, a numeric digit, an uppercase and a lowercase letter")
+    }
+
+    let noRefreshLogin = {
+        email : req.body.email,
+        password : req.body.password,
+    }
+
+    if(errors.length>0) {
+        res.render("login", {
+            title : "Login Page", 
+            errorMessages : errors,
+            retainData : noRefreshLogin
+        })
+    }
+
+    else {
+        res.redirect("/");
+    }
+ 
 });
 
 module.exports = router;
