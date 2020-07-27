f5817a2fc931afd015a2d553a15a0044f0e65404
Nishant Malik <Nishantmalik@hotmail.co.uk>
2020-07-27 10:32:21 -0400

    Merge Issue:routing part

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-const server = app.listen(PORT, () => {console.log(`Server running at http://localhost:${PORT}`)})
+const server = app.listen(PORT, () => { console.log(`Server running at http://localhost:${PORT}`) })
 const io = require('socket.io').listen(server)
 
 app.engine('handlebars', exphbs());
@@ -18,38 +18,31 @@ const generalController = require("./controllers/general");
 
 app.use("/", generalController);
 
-app.get("/chat",(req, res)=>{
 
-  res.render("chat", {
-      title : "Chat | BeMyPet"
+app.use(express.static('.'))
+
+
+
+io.on('connection', function (socket) {
+
+  // Listen for a "newuser" message
+  socket.on('newuser', (data) => {
+    // Transmit a message to everyone except the sender
+    //socket.broadcast.emit('newuser', data)
+
+    // The same message, sent to all users - try it!
+    io.emit('newuser', data)
   })
 
-});
-  
-  app.use(express.static('.'))
-  
-  
-  
-  io.on('connection', function (socket) {
-      
-    // Listen for a "newuser" message
-    socket.on('newuser', (data) => {
-      // Transmit a message to everyone except the sender
-      //socket.broadcast.emit('newuser', data)
-  
-      // The same message, sent to all users - try it!
-      io.emit('newuser', data)
-      })
-       
-      
-      // Listen for "chatmsg"
-      //   io.emit to all user
-      socket.on('chatmsg', (data) => {
-      socket.broadcast.emit('chatmsg', data)
-      })
-  
-  
+
+  // Listen for "chatmsg"
+  //   io.emit to all user
+  socket.on('chatmsg', (data) => {
+    socket.broadcast.emit('chatmsg', data)
   })
+
+
+})
 
 
 
