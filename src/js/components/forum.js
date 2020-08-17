b6ac814ec63d3fc219f4d7971790a7f959ea4c0e
manna84 <60120742+manna84@users.noreply.github.com>
2020-08-17 03:50:42 -0400

    styled component forum page

--- src/js/components/forum.js
+++ src/js/components/forum.js
@@ -30,6 +30,32 @@ padding: 3% 0;
 text-align: center;
 `
 
+const SectionAdoptH2 = styled.h2`
+    font-size: 2em;
+    text-transform: uppercase;
+    word-spacing: 4px;
+    letter-spacing: 2px;
+    text-align: center;
+    &::after {
+        display: table;
+        content: "";
+        clear: both;
+        height: .1em;
+        width: 25%;
+        background-color: #FC8121;
+        margin: .5em auto;
+    }
+`
+
+const SectionAdoptH3 = styled.h3`
+font-family: 'lato',Arial, Helvetica, sans-serif;
+    text-align: center;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    word-spacing: 2px;
+`
+}
+
 const SectionP = styled.p`
     width: 90%;
     margin: 0 auto;
@@ -70,6 +96,17 @@ margin-left: 1em;
     color: #FC8121;
 `
 
+const Dogimg = styled.img`
+margin: 2em;
+`
+const PetRow = styled.div`
+${Row}
+`
+
+const PostDiv = styled.div`
+margin-top: 2em;
+`
+
 const Forum = () => {
     return (
 
@@ -81,16 +118,16 @@ const Forum = () => {
             </HeadingDiv>
 
             <SectionAdopt>
-                <h2>Planning to adopt a Pet ?</h2>
+                <SectionAdoptH2>Planning to adopt a Pet ?</SectionAdoptH2>
                 <SectionP>Whether you choose to adopt an adult dog or a puppy, it’s a very personal decision that requires careful research and consideration of the type of home you will be able to provide. The advice and information below can help you make the right decision and find the perfect companion. </SectionP>
-                <img src={dog} alt="Dog red" />
+                <Dogimg src={dog} alt="Dog red" />
 
-                <div className="row" id="forum-head">
-                    <h3>want to put your pet for adoption ?</h3>
+                <PetRow id="forum-head">
+                    <SectionAdoptH3>want to put your pet for adoption ?</SectionAdoptH3>
                     <button type="submit" id="btn-post">GO ahead</button>
-                </div>
+                </PetRow>
 
-                <div id="post-box"></div>
+                <PostDiv id="post-box"></PostDiv>
 
             </SectionAdopt>
         </>
